e0e6b9750247a96472bc20a9952ff2f355a6be78
WallieWalrus805 <122319430+WallieWalrus805@users.noreply.github.com>
2024-05-23 14:37:53 -0700

    Update Calculator.py - no exponents and catch zero division

--- Calculator.py
+++ Calculator.py
@@ -6,12 +6,15 @@ def calculate():
     elif op == "*":
         return num1 * num2
     elif op == "/":
-        return num1 / num2
+        try:
+            return num1 / num2
+        except ZeroDivisionError:
+            return 'Can\'t divide by zero'
     else:
         return "Invalid operation, try again"
 
 
-print("This calculator supports the four basic operations and exponents.")
+print("This calculator supports the four basic operations.")
 try:
     num1 = float(input("Enter the first number: "))
 except ValueError:
